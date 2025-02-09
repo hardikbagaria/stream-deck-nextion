0b7a8b5b2562a0f85d777085c2654836655bf31b
hardik bagaria <61157326+hardikbagaria@users.noreply.github.com>
2025-02-09 14:21:24 +0530

    Update stream_deck.ino

--- stream_deck.ino
+++ stream_deck.ino
@@ -1,39 +1,92 @@
-#include <Nextion.h>
+#include <Keypad.h>
+#include <HID-Project.h>
 
-// Initialize the Nextion display
-Nextion myNextion(Serial);
+const byte ROWS = 4;
+const byte COLS = 4;
+char keys[ROWS][COLS] = {
+  {'1', '2', '3', 'A'},
+  {'4', '5', '6', 'B'},
+  {'7', '8', '9', 'C'},
+  {'*', '0', '#', 'D'}
+};
 
-// Define the buttons on the Nextion display
-#define BUTTON_1 "b0"  // Button 1 ID
-#define BUTTON_2 "b1"  // Button 2 ID
-#define BUTTON_3 "b2"  // Button 3 ID
+byte rowPins[ROWS] = {9, 8, 7, 6};
+byte colPins[COLS] = {5, 4, 3, 2}; 
+Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
 
 void setup() {
-  Serial.begin(9600);   // Start the Serial communication
-  myNextion.begin(9600); // Initialize Nextion
-  myNextion.sendCommand("page 0"); // Set to the first page of the Nextion display
+  Keyboard.begin();  // Initialize keyboard emulation
+  Consumer.begin();  // Initialize media controls
+  Serial.begin(9600);
+  delay(1000);
 }
 
 void loop() {
-  // Check for incoming data from the Nextion
-  myNextion.listen();
+  char key = keypad.getKey();
 
-  // Handle button presses
-  if (myNextion.getTouchValue() == 0) {
-    if (myNextion.getLastTouch() == BUTTON_1) {
-      // Action for Button 1
-      Serial.println("Button 1 Pressed");
-      // Add your code for Button 1 action here
-    } 
-    else if (myNextion.getLastTouch() == BUTTON_2) {
-      // Action for Button 2
-      Serial.println("Button 2 Pressed");
-      // Add your code for Button 2 action here
-    } 
-    else if (myNextion.getLastTouch() == BUTTON_3) {
-      // Action for Button 3
-      Serial.println("Button 3 Pressed");
-      // Add your code for Button 3 action here
+  if (key) {
+    switch (key) {
+      case '0':
+        Consumer.write(MEDIA_PLAY_PAUSE);
+        break;
+      case '#':
+        Consumer.write(MEDIA_PREVIOUS);
+        break;
+      case '*':
+        Consumer.write(MEDIA_NEXT);
+        break;
+      case '1':
+        Keyboard.press(KEY_F13);
+        Keyboard.release(KEY_F13);
+        break;
+      case '2':
+        Keyboard.press(KEY_F14);
+        Keyboard.release(KEY_F14);
+        break;
+      case '3':
+        Keyboard.press(KEY_F15);
+        Keyboard.release(KEY_F15);
+        break;
+      case '4':
+        Keyboard.press(KEY_F16);
+        Keyboard.release(KEY_F16);
+        break;
+      case '5':
+        Keyboard.press(KEY_F17);
+        Keyboard.release(KEY_F17);
+        break;
+      case '6':
+        Keyboard.press(KEY_F18);
+        Keyboard.release(KEY_F18);
+        break;
+      case '7':
+        Keyboard.press(KEY_F19);
+        Keyboard.release(KEY_F19);
+        break;
+      case '8':
+        Keyboard.press(KEY_F20);
+        Keyboard.release(KEY_F20);
+        break;
+      case '9':
+        Keyboard.press(KEY_F21);
+        Keyboard.release(KEY_F21);
+        break;
+      case 'A':
+        Serial.println("A");
+        break;
+      case 'B':
+        Serial.println("B");
+        break;
+      case 'C':
+        Serial.println("C");
+        break;
+      case 'D':
+        Serial.println("D");
+        break;
+      default:
+        Serial.println(key);
+        break;
     }
   }
+  delay(10);
 }
